35efcd9942d37b03a2ce34da1fbe5011f07a6832
unknown <irina.nicula0@gmail.com>
2024-10-17 16:27:53 +0200

    Added vuetify to project

--- product-selector/src/main.ts
+++ product-selector/src/main.ts
@@ -2,13 +2,25 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify';
 
 import App from './App.vue'
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 import router from './router'
+
+import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify);
 
 app.mount('#app')
