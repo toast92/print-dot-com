770fa06d297b9d53ee730a7b16a8f31e92db948a
Irina Nicula <irinanicula@Irinas-MacBook-Pro-2.local>
2024-10-19 10:10:19 +0200

    Added product store

--- product-selector/src/components/ProductSelector.vue
+++ product-selector/src/components/ProductSelector.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
-import { Poster, Flyer, BusinessCard } from '@/types/products';
+import { Poster, Flyer, BusinessCard } from '../types/productTypes';
 import { ref } from 'vue';
+import { useProductStore } from '../store/product';
+
+const productStore = useProductStore();
+const addToShoppingCart = (product) => {
+  productStore.shoppingCart.push(product);
+}
 const selectedValues = ref({});
 defineProps<{
   productData: Poster | Flyer | BusinessCard;
@@ -15,11 +21,11 @@ defineProps<{
         :label="property.title"
         :items="property.options"
         item-title="name"
-        item-value="slug"
+        item-value="name"
         :key="`${productData.type}-${property.slug}`"
         v-model="selectedValues[property.slug]"
       ></v-select>
-      <v-btn class="mt-2" type="submit" block>Submit</v-btn>
+      <v-btn class="mt-2" @click="addToShoppingCart(selectedValues)" block>Submit</v-btn>
     </v-form>
     <div>
       <h3>Selected Values:</h3>
