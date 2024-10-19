0a5976d39018618fa63e6eb2a9ebeeb0e6eefbab
unknown <irina.nicula0@gmail.com>
2024-10-19 14:32:55 +0200

    Added posters as default route

--- product-selector/src/router/index.ts
+++ product-selector/src/router/index.ts
@@ -3,6 +3,9 @@ import { createRouter, createWebHistory } from 'vue-router';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: '/',
+      component: () => import('../views/PostersView.vue')
+    },
     {
       path: '/posters',
       name: 'posters',
